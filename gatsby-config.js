757fc5ec0ee267774122ed5e4f5445a5698b2a90
LekoArts <lekoarts@gmail.com>
2020-12-07 12:18:24 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
