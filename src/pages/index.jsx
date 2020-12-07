afd9fb42abeb19fe166b91be1baebc69c1dc9b69
LekoArts <lekoarts@gmail.com>
2020-12-07 21:05:43 +0100

    Use components in pages & add Instagram pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,37 @@
 import * as React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
+import Wrapper from "../components/wrapper"
+import Insta from "../components/insta"
+import Greetings from "../components/greetings"
 
 const IndexPage = ({ data }) => {
   const notes = data.allMarkdownRemark.nodes
+  const instagram = data.allInstaNode.nodes
   return (
-    <main className="prose prose-sm sm:prose lg:prose-lg xl:prose-xl max-w-none sm:max-w-none lg:max-w-none xl:max-w-none">
-      <h2>Notes</h2>
-      <ul>
-        {notes.map((note) => (
-          <li key={note.frontmatter.title}>{note.frontmatter.title}</li>
-        ))}
-      </ul>
-    </main>
+    <Wrapper>
+      <main className="prose prose-sm sm:prose lg:prose-lg xl:prose-xl max-w-none sm:max-w-none lg:max-w-none xl:max-w-none">
+        <h2>Notes</h2>
+        <ul>
+          {notes.map((note) => (
+            <li key={note.frontmatter.title}>
+              <Link to={note.gatsbyPath}>{note.frontmatter.title}</Link>
+            </li>
+          ))}
+        </ul>
+        <Greetings />
+        <h2>Instagram</h2>
+        <div className="grid gap-8 grid-cols-2 index-page-grid">
+          {instagram.map((i) => (
+            <Insta
+              key={i.id}
+              caption={i.caption}
+              timestamp={i.timestamp}
+              image={i.localFile.childImageSharp.gatsbyImageData}
+            />
+          ))}
+        </div>
+      </main>
+    </Wrapper>
   )
 }
 
@@ -23,6 +43,26 @@ export const query = graphql`
       nodes {
         frontmatter {
           title
+        }
+        gatsbyPath(filePath: "/notes/{MarkdownRemark.frontmatter__title}")
+      }
+    }
+    allInstaNode(sort: { fields: timestamp, order: DESC }) {
+      nodes {
+        id
+        caption
+        timestamp
+        localFile {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 90
+              maxWidth: 410
+              layout: CONSTRAINED
+              placeholder: DOMINANT_COLOR
+              transformOptions: { cropFocus: CENTER }
+              maxHeight: 410
+            )
+          }
         }
       }
     }
